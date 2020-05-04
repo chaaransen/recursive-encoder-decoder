4d66fff442757922f9c24aa3aa83e230242e823c
Chaaran Sen <chaaran.sen@gmail.com>
2020-05-04 21:17:11 +0530

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-const base64 = require('./index.js');
-
-let str = "Hello world";
-
-let encodeStr = base64.encodeRecursive(str, 10, 'ascii', true);
-
-console.log(base64.decodeRecursive(encodeStr, 10, 'ascii', true));
